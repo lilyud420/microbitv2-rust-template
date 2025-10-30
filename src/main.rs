f2bd181d72cbfbca84b1fa66110f5f3fa32f7b89
lilyud69420 <duycvvn3@gmail.com>
2025-10-30 16:06:59 +0700

    feat: add blinking led example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
 #![no_std]
 #![no_main]
 
-// cortex-m
-// cortex-m-rt
-// embedded-hal
-// microbit-v2
-// panic-rtt-target
-// rtt-target
+/*
+cortex-m
+cortex-m-rt
+embedded-hal
+microbit-v2
+panic-rtt-target
+rtt-target
+*/
 
+use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::hal::{gpio, timer};
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+
+
+// Example: blinking led
 #[entry]
-fn main() -> ! {}
+fn main() -> ! {
+    rtt_init_print!();
+
+    let board = microbit::Board::take().unwrap();
+    let mut timer = timer::Timer::new(board.TIMER0);
+
+    let mut row3 = board
+        .display_pins
+        .row3
+        .into_push_pull_output(gpio::Level::High);
+
+    let mut _col3 = board
+        .display_pins
+        .col3
+        .into_push_pull_output(gpio::Level::Low);
+
+    loop {
+        row3.set_high().unwrap();
+        timer.delay_ms(500);
+        row3.set_low().unwrap();
+        timer.delay_ms(500);
+    }
+}
